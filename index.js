338538491689dc5eb3314f2e97a6959119adb38c
ghospadi <Lolr2223@gmail.com>
2022-08-08 19:03:49 +0300

    router

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 import express from "express";
+import { userRouter } from "./users/users.js";
 
 const port = 8000;
 const app = express();
 
 app.get("/hello", (req, res) => {
-  res.set("Content-Type", "text/plain");
-  res.append("Warning", "code");
-  res.type("html");
-  res.location("");
-  res.links({
-    next: "dsfaf",
-  });
-  res.cookie("token", "dsdsdssd", {});
-  res.clearCookie("token");
   res.send("Hello");
   res.end();
 });
+
+app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
