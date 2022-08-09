b35f4943e8f03d0e8f516fb35b0ef6c4d37110f9
ghospadi <Lolr2223@gmail.com>
2022-08-09 16:41:46 +0300

    union type

--- test.ts
+++ test.ts
@@ -1,29 +1,25 @@
-let a = 5;
-let b: string = "1";
+let universalId: number | string = "2";
+universalId = 5;
 
-let c: number = a + Number(b);
-
-let d = true;
-
-let names: string[] = ["sd", "dsdsds"];
-let ages: number[] = [4, 10];
-
-let tup: [number, string] = [1, "dsada"];
-tup.push("");
-
-let e: any = 5;
-e = "dsa";
-e = true;
-
-let anyArr: any[] = ["dsa", 3, true];
-
-function greet(name: string): string {
-  return name + "Hi";
-}
-
-names.map((x: string) => x);
-
-function coord(coord: { lat: number; lon?: number }) {
-  if (coord.lon !== null) {
+function printId(id: number | string) {
+  if (typeof id == "string") {
+    console.log(id.toUpperCase());
+  } else {
+    console.log(id);
   }
 }
+
+function helloUser(user: string | string[]) {
+  if (Array.isArray(user)) {
+    const filterUser = user.filter((user) => user.toLowerCase() === "vitya");
+    filterUser.length !== 0 ? console.log("Vitya") : console.log("not found");
+  } else {
+    if (user.toLowerCase() === "andrey") {
+      console.log("Andrey");
+    } else {
+      console.log("Not found");
+    }
+  }
+}
+
+helloUser(["Andrey", "artem", "tymur"]);
