5d821c423e4bb925883dea649ef3205e84a7efe0
ghospadi <Lolr2223@gmail.com>
2022-08-09 21:41:44 +0300

    classes

--- test.ts
+++ test.ts
@@ -1,43 +1,125 @@
-function log<T>(obj: T): T {
-  console.log(obj);
-  return obj;
+class Coord {
+  lat: number;
+  lng: number;
+
+  computeDistance(newLat: number, newLng: number): number {
+    return 0;
+  }
+
+  protected test(lat: number): number {
+    if (this.lat > 0) {
+      return 0;
+    }
+    return 1;
+  }
+
+  constructor(lat: number, lng: number) {
+    this.lat = lat;
+    this.lng = lng;
+  }
 }
 
-log<string>("1321");
-log<number>(1);
+/////////////////////////////////////////////
 
-function log1<T, K>(obj: T, arr: K[]): K[] {
-  console.log(obj);
-  return arr;
+const first = new Coord(1, 1);
+
+class MapLocation extends Coord {
+  private _name: string;
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(s: string) {
+    this._name = s + "_cool";
+  }
+
+  override computeDistance(newLat: number, newLong: number): number {
+    console.log(this._name);
+    this.test(1);
+    return 1;
+  }
+
+  constructor(lat: number, lng: number, name: string) {
+    super(lat, lng);
+    this._name = name;
+  }
 }
 
-log1<string, number>("dsada", [1]);
-
-interface HasLength {
-  length: number;
+const loc = new MapLocation(0, 1, "dsa");
+if (loc.name) {
+  loc.name = "dsadasdsa";
 }
 
-function log2<T extends HasLength, K>(obj: T, arr: K[]): K[] {
-  obj.length;
-  return arr;
+console.log(loc.name);
+
+//////////////////////////////////////////////
+
+interface LoggerService {
+  log: (s: string) => void;
 }
 
-interface IUser {
+class Logger implements LoggerService {
+  public log(s: string) {
+    console.log(s);
+  }
+  private log1(s: string) {
+    console.log(s);
+  }
+  #a = 1;
+}
+
+const l = new Logger();
+
+l.log("1");
+
+//////////////////////////////////////////////
+
+class MyClass {
+  static a = 1;
+  static {}
+}
+
+MyClass.a;
+
+//////////////////////////////////////////////
+
+class MyClass2<T> {
+  a: T;
+}
+
+const b = new MyClass2<string>;
+b.a;
+
+///////////////////////////////////////////////
+
+abstract class Base {
+  print(s: string): void {
+    console.log(s);
+  }
+
+  abstract error(s: string): void;
+}
+
+class BaseExtended extends Base {
+  error(s: string) {
+
+  }
+}
+
+new BaseExtended().print('s');
+
+////////////////////////////////////////////////
+
+class Animal {
   name: string;
-  age?: number;
-  bid: (sum: number) => boolean;
 }
 
-function bid(sum: number): boolean {
-  return true;
-}
-
-interface IUser2 {
+class Dog {
   name: string;
-  age?: number;
-  bid2: <T>(sum: T) => boolean;
+  tail: boolean;
 }
 
-function bid2<T>(sum: T): boolean {
-  return true;
-}
+const puppy: Animal = new Dog();
+
+////////////////////////////////////////////////
